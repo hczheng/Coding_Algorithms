9440a33a80a23973b5fcb7d3b48d3bf61f33570a
zhenghuangcheng <zhc19931201@gmail.com>
2016-12-18 14:05:12 +0800

    add ppi_and_ipp

--- ppi_and_ipp/ppi_and_ipp_vs.cpp
+++ ppi_and_ipp/ppi_and_ipp_vs.cpp
@@ -5,22 +5,11 @@ using namespace std;
 
 int main(void)
 {
-	//int i = 0;
-	//cout << "Test execute order: " << endl
-	//	<< "cout: "
-	//	<< i << ", " << ++i <<", " << ++i + ++i << ", " << ++i + ++i + ++i << endl;//6,6,12,18
-
-	//i = 0;
-	//cout << "Test execute order: " << endl
-	//	<< "cout: "
-	//	<< i << ", " << ++i << ", " << ++i + ++i << ", " << ++i + ++i + i++ << endl;//6,6,12,6
-
+	
 	//int i = 0;
 	//printf("printf(): %d, %d,%d,%d\n", i, ++i, ++i + ++i, ++i + ++i + ++i);//6,6,12,18
 	int i = 0;
-	//printf("printf(): %d,%d,%d,%d,%d,%d\n", i,i++, ++i, ++i + ++i, i++ + i++, ++i + ++i + i++);//7,6,7,14,6
 	printf("%d %d %d %d %d\n", i, i++, ++i, ++i + ++i, ++i + ++i + i++);//7 6 7 10 6
-
 	getchar();
 }
 
